ab84b987fd5a440e330563fb55748e170a4fdca5
Satvik Patil <sapatil@barracuda.com>
2026-08-25 14:43:28 +0530

    graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Satvikmpatil/Student-Management-REST-API-Fast-Scalable/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+
+	//setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("WELCOME STUDENT"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("Server Started %s",cfg.Addr)
+	fmt.Printf("Server Started %s",cfg.Addr)
+	
+	done := make(chan os.Signal,1)
+
+	signal.Notify(done, os.Interrupt,syscall.SIGABRT,syscall.SIGTERM)
+
+	go func(){
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Error",err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel:= context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil{
+		slog.Error("Failed ", slog.String("error",err.Error()))
+	}
+
+	slog.Info("Server shutdown")
+	
 }
